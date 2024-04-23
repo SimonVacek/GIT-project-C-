37f7c762b4b2c1ee2cfd817ff53d2a57f40ce6b9
Simon Vacek <simon.vacek@student.ssnd.sk>
2024-04-23 19:12:58 +0200

    Milliseconds counting

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
+
+void milliseconds() {
+    cout << "Aktualny cas od 1. januara 1970: " << time(NULL)*1000 << " ms" << endl;
+}
 
 int main() {
     
