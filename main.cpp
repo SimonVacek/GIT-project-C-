d1b790268c443b4689a9b00ed25e3774e1ab9643
Boris Bobacik <boris.bobacik@student.ssnd.sk>
2024-04-23 20:12:30 +0200

    Function prints names of authors

--- main.cpp
+++ main.cpp
@@ -6,7 +6,11 @@ void milliseconds() {
     cout << "Aktualny cas od 1. januara 1970: " << time(NULL)*1000 << " ms" << endl;
 }
 
+void autori() {
+    cout << "Autori tohto projektu: Simon Vacek, Boris Bobacik." << endl;
+}
+
 int main() {
-    
+
     return 0;
 }
