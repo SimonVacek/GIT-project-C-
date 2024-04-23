df79efd10986af9daac79b36cdb4ef63b362cdfb
Simon Vacek <simon.vacek@student.ssnd.sk>
2024-04-23 19:51:41 +0200

    Bubble sort implemented

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 #include <ctime>
+
 using namespace std;
+
+#define SIZE (20)
+
+int pole[SIZE] = {5, 81, 285, 1, 47, 3, 2, 20, 7, 63, 0, -1, 555, 8, 68, 9, 41, -14, -600, 4989};
 
 void milliseconds() {
     cout << "Aktualny cas od 1. januara 1970: " << time(NULL)*1000 << " ms" << endl;
+}
+
+void bubble_sort() {
+    for (int bublanie = 0; bublanie < SIZE - 1; bublanie++) {
+        for (int i = 0; i < SIZE - bublanie - 1; i++) {
+            if (pole[i] > pole[i + 1]) {
+                swap(pole[i], pole[i + 1]);
+            }
+        }
+    }
 }
 
 int main() {
