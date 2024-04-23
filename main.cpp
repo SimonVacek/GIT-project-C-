b64afcbd2a4b2864be7b0477774df1a442268a0b
Simon Vacek <simon.vacek@student.ssnd.sk>
2024-04-23 20:06:48 +0200

    Insertion sort implemented

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 #include <ctime>
+
 using namespace std;
+
+#define SIZE (20)
+
+int pole[SIZE] = {5, 81, 285, 1, 47, 3, 2, 20, 7, 63, 0, -1, 555, 8, 68, 9, 41, -14, -600, 4989};
 
 void milliseconds() {
     cout << "Aktualny cas od 1. januara 1970: " << time(NULL)*1000 << " ms" << endl;
+}
+
+void insertion_sort() {
+    int i, key, j;
+    for (i = 1; i < SIZE; i++) {
+        key = pole[i];
+        j = i - 1;
+        while (j >= 0 && pole[j] > key) {
+            pole[j + 1] = pole[j];
+            j = j - 1;
+        }
+        pole[j + 1] = key;
+    }
 }
 
 int main() {
